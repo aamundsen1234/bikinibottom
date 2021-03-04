f3b16b01c9e0cedc0ea3484749c5d22e43c1ed81
Alan Amundsen <aamundsen@burwood.com>
2021-03-03 19:11:02 -0600

    updated main.py

--- hello_world/main.py
+++ hello_world/main.py
@@ -1,16 +1,10 @@
-def hello_world(request, context):
-    """Responds to any HTTP request.
+import base64
+
+def hello_pubsub(event, context):
+    """Triggered from a message on a Cloud Pub/Sub topic.
     Args:
-        request (flask.Request): HTTP request object.
-    Returns:
-        The response text or any set of values that can be turned into a
-        Response object using
-        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
+         event (dict): Event payload.
+         context (google.cloud.functions.Context): Metadata for the event.
     """
-    request_json = request.get_json()
-    if request.args and 'message' in request.args:
-        return request.args.get('message')
-    elif request_json and 'message' in request_json:
-        return request_json['message']
-    else:
-        return f'Hello World!'
+    pubsub_message = base64.b64decode(event['data']).decode('utf-8')
+    print(pubsub_message)
